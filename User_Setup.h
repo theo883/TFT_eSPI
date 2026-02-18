9cff4c993e536e02c0275ee4e4bca7735b332295
theo883 <theodoor.seigers@telenet.be>
2026-02-18 13:57:09 +0100

    Update display driver definitions and pin assignments

--- User_Setup.h
+++ User_Setup.h
@@ -44,7 +44,7 @@
 // Only define one driver, the other ones must be commented out
 #define ILI9341_DRIVER       // Generic driver for common displays
 //#define ILI9341_2_DRIVER     // Alternative ILI9341 driver, see https://github.com/Bodmer/TFT_eSPI/issues/1172
-//#define ST7735_DRIVER      // Define additional parameters below for this display
+#define ST7735_DRIVER      // Define additional parameters below for this display
 //#define ILI9163_DRIVER     // Define additional parameters below for this display
 //#define S6D02A1_DRIVER
 //#define RPI_ILI9486_DRIVER // 20MHz maximum SPI
@@ -107,7 +107,7 @@
 // #define ST7735_GREENTAB160x80 // For 160 x 80 display (BGR, inverted, 26 offset)
 // #define ST7735_ROBOTLCD       // For some RobotLCD Arduino shields (128x160, BGR, https://docs.arduino.cc/retired/getting-started-guides/TFT)
 // #define ST7735_REDTAB
-// #define ST7735_BLACKTAB
+ #define ST7735_BLACKTAB
 // #define ST7735_REDTAB160x80   // For 160 x 80 display with 24 pixel offset
 
 // If colours are inverted (white shows as black) then uncomment one of the next
@@ -167,14 +167,14 @@
 // ###### EDIT THE PIN NUMBERS IN THE LINES FOLLOWING TO SUIT YOUR ESP8266 SETUP ######
 
 // For NodeMCU - use pin numbers in the form PIN_Dx where Dx is the NodeMCU pin designation
-#define TFT_MISO  PIN_D6  // Automatically assigned with ESP8266 if not defined
-#define TFT_MOSI  PIN_D7  // Automatically assigned with ESP8266 if not defined
-#define TFT_SCLK  PIN_D5  // Automatically assigned with ESP8266 if not defined
+#define TFT_MISO   // Automatically assigned with ESP8266 if not defined
+#define TFT_MOSI  4  // Automatically assigned with ESP8266 if not defined
+#define TFT_SCLK  5  // Automatically assigned with ESP8266 if not defined
 
-#define TFT_CS    PIN_D8  // Chip select control pin D8
-#define TFT_DC    PIN_D3  // Data Command control pin
+#define TFT_CS    7  // Chip select control pin D8
+#define TFT_DC    6  // Data Command control pin
 #define TFT_RST   PIN_D4  // Reset pin (could connect to NodeMCU RST, see next line)
-//#define TFT_RST  -1     // Set TFT_RST to -1 if the display RESET is connected to NodeMCU RST or 3.3V
+#define TFT_RST  2     // Set TFT_RST to -1 if the display RESET is connected to NodeMCU RST or 3.3V
 
 
 //#define TFT_BL PIN_D1  // LED back-light (only for ST7789 with backlight control pin)
